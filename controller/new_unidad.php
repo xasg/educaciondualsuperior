cc5452802d40897d2131244ba31cb47ed84a80ef
Adrian Soto <xasg@hotmail.com>
2023-05-24 14:58:44 -0600

    Fix new_unidad_academica es decir economica

--- controller/new_unidad.php
+++ controller/new_unidad.php
@@ -11,12 +11,26 @@
    $estudiante_sex_mas = isset( $_POST['estudiante_sex_mas']) ? $_POST['estudiante_sex_mas'] : '';
    $docente_sex_fem = isset( $_POST['docente_sex_fem']) ? $_POST['docente_sex_fem'] : '';
    $docente_sex_mas = isset( $_POST['docente_sex_mas']) ? $_POST['docente_sex_mas'] : '';
-   $egresados = isset( $_POST['egresados']) ? $_POST['egresados'] : '';
-   $egresados_f = isset( $_POST['egresados_f']) ? $_POST['egresados_f'] : '';
-   $egresados_m = isset( $_POST['egresados_m']) ? $_POST['egresados_m'] : '';
-   $estudiante_cursan_f = isset( $_POST['estudiante_cursan_f']) ? $_POST['estudiante_cursan_f'] : '';
-   $estudiante_cursan_m = isset( $_POST['estudiante_cursan_m']) ? $_POST['estudiante_cursan_m'] : '';
-   $beneficio = implode(', ', $_POST['beneficio']);
+   /**
+    * Validamos si no hay egresados los valores de egresados_f y egresados_m se establescan en cero 
+    */
+   $egresados = isset( $_POST['egresados']) ? $_POST['egresados'] : 'No';
+   if ($egresados == 'No'){
+      $egresados_f = 0;
+      $egresados_m = 0;
+
+   }else{
+      $egresados_f = isset( $_POST['egresados_f']) ? $_POST['egresados_f'] : 0;
+      $egresados_m = isset( $_POST['egresados_m']) ? $_POST['egresados_m'] : 0;
+   }
+   $estudiante_cursan_f = isset( $_POST['estudiante_cursan_f']) ? $_POST['estudiante_cursan_f'] : 0;
+   $estudiante_cursan_m = isset( $_POST['estudiante_cursan_m']) ? $_POST['estudiante_cursan_m'] : 0;
+   if(isset( $_POST['beneficio'])){
+      $beneficio = implode(', ', $_POST['beneficio']);
+   }else{
+      $beneficio = 'Ninguno';
+   }
+   
    crear_unidad($id, $unidad, $convn, $estudiante_sex_fem, $estudiante_sex_mas, $docente_sex_fem, $docente_sex_mas, $egresados, $egresados_f, $egresados_m, $estudiante_cursan_f, $estudiante_cursan_m, $beneficio);
   
    

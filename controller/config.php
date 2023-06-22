7fc63dcb085f7d10805b18717233f46c61ac664c
reivajfly <francisco@fese.mx>
2023-06-22 13:28:08 -0600

    se cambio los datos de conexion del archivo conf.php

--- controller/config.php
+++ controller/config.php
@@ -1,8 +1,18 @@
 <?php
-define('DB_SERVER', 'localhost');
+/*define('DB_SERVER', 'localhost');
 define('DB_SERVER_USERNAME', 'root');
 define('DB_SERVER_PASSWORD', '');
+define('DB_DATABASE', 'educaciondual');*/
+
+
+
+define('DB_SERVER', 'ec2-18-216-120-87.us-east-2.compute.amazonaws.com');
+define('DB_SERVER_USERNAME', 'fese');
+define('DB_SERVER_PASSWORD', 'Ws.666.AdQe');
 define('DB_DATABASE', 'educaciondual');
+
+
+
 
 $connexion = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
 ?>

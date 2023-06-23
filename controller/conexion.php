ec425a53a32a548eff3d48b69125d296ab64c461
reivajfly <francisco@fese.mx>
2023-06-23 11:00:57 -0600

    se cambio la conexion de conexion.php

--- controller/conexion.php
+++ controller/conexion.php
@@ -1,22 +1,21 @@
-<?php  
-
-// Esta conexion es la que esta en la rama de prod y se va a cambiar por la de dev
+<?php 
 //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	/*$mysqli = new mysqli("localhost","fese","Ws.666.AdQe","educacion_dual"); 
+	$mysqli = new mysqli("ec2-18-216-120-87.us-east-2.compute.amazonaws.com","fese","Ws.666.AdQe","educacion_dual"); 
 	
-	if(mysqli_connect_errno()){
-		echo 'Conexion Fallida : ', mysqli_connect_error();
-		exit();
-	}*/
-?>
-
-<?php
-//servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-// esta es la conexion que tenemos en dev
-	$mysqli = new mysqli("localhost","root","","educaciondual"); 
-	
-	if(mysqli_connect_errno()){
+	if(mysqli_connect_errno())
+	{
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
 	}
+?>
+
+<?php
+// dejamos la conexion de pro
+// esta es la conexion que tenemos en dev 	
+	/*$mysqli = new mysqli("localhost","root","","educaciondual"); 	
+	if(mysqli_connect_errno())
+	{
+		echo 'Conexion Fallida : ', mysqli_connect_error();
+		exit();
+	}*/
 ?>

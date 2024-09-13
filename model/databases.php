f399f9956695568f569a38fe3ecc5f16a2ac2c28
rmendezg <rodrigo.mendez26@gmail.com>
2024-09-13 09:09:27 -0600

    error reporte
    errores  reporte

--- model/databases.php
+++ model/databases.php
@@ -148,7 +148,7 @@ function acces_report_21()
    LEFT JOIN unidad_educativa ON(programa_educativo.id_programa = unidad_educativa.id_programa_educativo)
    where usuarios.dt_tipo=2 AND nombre_entidad IS  NOT NULL  AND inf_ciclo='2021-2022'
    GROUP BY dt_correo ORDER BY nombre_entidad ASC";
-  return $mysqli->query($sql) or die(mysql_error()); 
+  return $mysqli->query($sql); 
   return $result->fetch_assoc();
 }
 

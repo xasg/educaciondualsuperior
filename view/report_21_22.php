ce0de92af3b1a6b8c9026deb0b77b8f7effe1ada
rmendezg <rodrigo.mendez26@gmail.com>
2024-09-13 00:04:39 -0600

    Update report_21_22.php
    prueba solucionar error consulta

--- view/report_21_22.php
+++ view/report_21_22.php
@@ -7,7 +7,7 @@ require_once('../model/databases.php');
 mysqli_set_charset( $mysqli, 'utf8');
 /**$id_ies=$_SESSION["id_ies"];**/
 $name_user=$_SESSION["name_user"];
-$responsable = acces_report_21();
+$responsable = view_entidad();
 $rep_ies = rep_ies();
 $ies=$rep_ies['ies'];
 $rep_programas = rep_programas();

b02cd434ec335a9b488745dc41c70b196cdf568e
rmendezg <rodrigo.mendez26@gmail.com>
2024-09-13 00:34:28 -0600

    Update databases.php
    Revisando error en consulta reporte_21

--- model/databases.php
+++ model/databases.php
@@ -135,21 +135,7 @@ function acces_responsables()
 function acces_report_21()
 {
   global $mysqli;
- $sql = "SELECT nombre_entidad,dt_nombre_ies,cat_subsistema.dt_nombre_subsistema,dt_correo,
-   COUNT(DISTINCT programa_educativo.id_programa) as programas_educativos,
-   COUNT(unidad_educativa.id_programa_educativo) as unidad_economica,
-   SUM(unidad_educativa.dt_estudiante_fem + unidad_educativa.dt_estudiante_mas) as estudiantes_2021_2022,
-   SUM(unidad_educativa.dt_egresados_fem + unidad_educativa.dt_egresados_mas) as egresados,
-   SUM(unidad_educativa.dt_estudiante_cursan_f + unidad_educativa.dt_estudiante_cursan_m) as estudiantes_2022_2023
-   FROM usuarios
-   LEFT JOIN responsable ON(responsable.id_usuario=usuarios.id_usuario)
-   LEFT JOIN cat_ies ON(cat_ies.id_ies=usuarios.id_ies)
-   LEFT JOIN cat_subsistema ON(cat_subsistema.id_cat_subsistema = cat_ies.id_cat_subsistema)
-   LEFT JOIN cat_entidad ON(cat_entidad.id_cat_entidad = responsable.dt_entidad)
-   LEFT JOIN programa_educativo ON(programa_educativo.id_usuario = responsable.id_usuario)
-   LEFT JOIN unidad_educativa ON(programa_educativo.id_programa = unidad_educativa.id_programa_educativo)
-   where usuarios.dt_tipo=2 AND nombre_entidad IS  NOT NULL  AND inf_ciclo='2021-2022'
-   GROUP BY dt_correo ORDER BY nombre_entidad ASC";
+ $sql = "SELECT * FROM cat_entidad";
   return $mysqli->query($sql); 
   return $result->fetch_assoc();
 }

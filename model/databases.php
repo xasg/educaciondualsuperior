0bb28b0ab823b8f9477804189d0e697cd1d843d6
rmendezg <rodrigo.mendez26@gmail.com>
2024-09-13 08:47:56 -0600

    Update databases.php
    prueba errores

--- model/databases.php
+++ model/databases.php
@@ -149,8 +149,8 @@ function acces_report_21()
    LEFT JOIN programa_educativo ON(programa_educativo.id_usuario = responsable.id_usuario)
    LEFT JOIN unidad_educativa ON(programa_educativo.id_programa = unidad_educativa.id_programa_educativo)
    where usuarios.dt_tipo=2 AND nombre_entidad IS  NOT NULL  AND inf_ciclo='2021-2022'
-   ORDER BY nombre_entidad ASC";
-  return $mysqli->query($sql); 
+   GROUP BY dt_correo ORDER BY nombre_entidad ASC";
+  return $mysqli->query($sql) or die(mysql_error()); 
   return $result->fetch_assoc();
 }
 

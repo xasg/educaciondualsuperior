535b0419f55573058ea8cf576c310077188fc4c8
rmendezg <rodrigo.mendez26@gmail.com>
2024-11-12 11:49:49 -0600

    modificacion conexion
    modificacion conexion

--- controller/conec.php
+++ controller/conec.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $dbname = "educacion_dual";
-$username = "root";
-$password = "";
+$username = "fese";
+$password = "Ws.666.AdQe";
 
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 $result ='';
